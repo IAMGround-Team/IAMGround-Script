3e5da67bbe24fda4fc64afa60a3241fed1f14b98
rudghk <7942jgh@gmail.com>
2021-12-11 23:49:15 +0900

    [FIX] Monitor yaml 제거, requestParameters 예외 처리

--- Monitor.py
+++ Monitor.py
@@ -2,7 +2,6 @@
 import boto3 
 import sys
 import json
-import yaml
 import copy
 import ServiceAction as sa
 import Convert as cvt
@@ -14,38 +13,16 @@ def get_reqeust_target(requestParameters):
         if 'policy' not in key:
             return value
 
-# service or action에 * 존재 여부 확인
-def check_action_exist_asterisk(targets):
+# targets(list or string)에 * 존재 여부 확인
+def check_exist_asterisk(targets):
     asterisk_flag = False
     if isinstance(targets, list):
-        for permission in targets:
-            if permission == '*':
-                asterisk_flag = True
-                break
-            else:
-                action = permission.split(":")[-1]
-                if action == '*':
-                    asterisk_flag = True
-                    break     
-    else:
-        if targets == '*':
-            asterisk_flag = True
-        else:
-            action = targets.split(":")[-1]
-            if action == '*':
-                asterisk_flag = True
-    return asterisk_flag
-
-# resource에 * 존재 여부 확인
-def check_resource_exist_asterisk(targets):
-    asterisk_flag = False
-    if isinstance(targets, list):
-        for resource in targets:
-            if resource == '*':
+        for action in targets:
+            if '*' in action:
                 asterisk_flag = True
                 break
     else:
-        if targets == '*':
+        if '*' in targets:
             asterisk_flag = True
     return asterisk_flag
 
@@ -53,7 +30,7 @@ def check_resource_exist_asterisk(targets):
 def check_policy(document, related):
     actionAsterisk_flag = False # * 사용(False == secure)
     resourceAsterisk_flag = False # * 사용(False == secure)
-    no_condition_flag = False # 조건 사용 안함(False == secure)
+    no_condition_flag = True # 조건 사용 안함(False == secure)
     no_deny_flag = True # Deny 명시 안함(False == secure)
     allowNotAction_flag = False # Effect:Allow NotAction 조합 사용(False == secure)
 
@@ -65,18 +42,17 @@ def check_policy(document, related):
         effect = statement['Effect']
         if effect == 'Allow':
             if 'Action' in statement.keys():
-                if actionAsterisk_flag == False:
-                    actions = statement['Action']
-                    actionAsterisk_flag = check_action_exist_asterisk(actions)
-                if resourceAsterisk_flag == False and 'Resource' in statement.keys():
+                actions = statement['Action']
+                actionAsterisk_flag = check_exist_asterisk(actions)
+                if 'Resource' in statement.keys():
                     resources = statement['Resource']
-                    resourceAsterisk_flag = check_resource_exist_asterisk(resources)        
+                    resourceAsterisk_flag = check_exist_asterisk(resources)          
             if 'NotAction' in statement.keys():
                 allowNotAction_flag = True
         elif effect == 'Deny':
             no_deny_flag = False
-        # if 'Condition' in statement.keys():
-        #     no_condition_flag = False
+        if 'Condition' in statement.keys():
+            no_condition_flag = False
     flagList = [actionAsterisk_flag, resourceAsterisk_flag, no_condition_flag, no_deny_flag, allowNotAction_flag]
     for idx in range(len(flagList)):
         if flagList[idx] == True:
@@ -254,15 +230,15 @@ def check_policy_by_version(policyKey, pastVersionId, lastVersionId, aws, scan, 
 if __name__ == '__main__':
     if len(sys.argv) <= 6:
         sys.stderr.write("인자가 충분하지 않음\n")
+        f = open('./src/scripts/' + sys.argv[6] + '.json', 'w')
+        f.write('error')
+        f.close()
         exit(0)
     
     scan = ScanInfo()
     aws = AWSIAMInfo()
-    aws.create_iam_client(sys.argv[1], sys.argv[2], sys.argv[3])
-    try:
-        event = json.loads(sys.argv[4])
-    except:
-        event = yaml.load(sys.argv[4])
+    aws.create_iam_client(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[6])
+    event = json.loads(sys.argv[4])
     aws.orgTable = json.loads(sys.argv[5])
     
     eventSource = event['detail']['eventSource']
@@ -302,7 +278,6 @@ if __name__ == '__main__':
             log['resource_name'].append(value)
     except:
         pass
-    
 
     related = {}
 
